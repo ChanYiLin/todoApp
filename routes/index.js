0e98de75e7b73fec78d02e4e7fe246a1a0ef62a9
ChanYiLin <j5111261112@gmail.com>
2016-08-06 16:37:40 +0800

    finish RESTful API

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,135 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Models
+var Task = require('../models').Task;
+/*
+	name: String,
+	done: Boolean,
+	note: String,
+	createAt:{
+		type: Date,
+		default: Date.now
+	},
+	updateAt:{
+		type: Date,
+  		default: Date.now
+	}
+
+*/
+
+/* GET / show_list */
+router.get('/', function(req, res, next){
+	Task.find()
+		.sort({
+			createdAt: 'desc'
+		})
+		.
+		exec(function(err, items){
+			if (err) console.log(err);
+
+			res.render('index', {
+				tasks: tasks
+			});
+		});
+});
+
+/* GET /:id show_task */
+router.get('/:id/show', function(req, res, next){
+	var id = req.param.id;
+	Task.findById(id, function(err,task){
+		if(err) console.log(err);
+		res.json(task);
+	});
+});
+
+/* 
+	POST / create 
+*/
+router.post('/', function(req, res, next){
+	var name = req.body.name;
+	var note = req.body.note;
+	
+	var task = new Task({
+		name: name,
+		done: false,
+		note: note
+	});
+
+	task.save(function(err){
+		if (err) console.log(err);
+
+		res.redirect('/');
+	});
+});
+
+/* 
+	Done
+	PUT /:id update 
+*/
+router.put('/:id/done', function(req, res, next){
+	var id = req.param.id;
+	Task.findOneAndUpdate({
+		_id: id;
+	},{
+		done: true;
+	},function(err, task){
+		if(err) console.log(err);
+
+		console.log(task);
+		res.json(true);
+	});
+});
+
+/* 
+	UnDone
+	PUT /:id update 
+*/
+router.put('/:id/undone', function(req, res, next){
+	var id = req.param.id;
+	Task.findOneAndUpdate({
+		_id: id;
+	},{
+		done: false;
+	},function(err, task){
+		if(err) console.log(err);
+		
+		console.log(task);
+		res.json(true);
+	});
+});
+
+/* 
+	Update
+	PUT /:id update 
+*/
+router.put('/:id/update', function(req, res, next){
+	var id = req.param.id;
+
+	var note = req.body;
+	Task.findOneAndUpdate({
+		_id: id
+	},{
+		note: note
+	},function(err, task){
+		if(err) console.log(err);
+		
+		console.log(task);
+		res.json(true);
+	});
 });
 
 
-// Index GET
+/* Delete /:id delete */
+router.delete('/:id/delete', function(req, res, next){
+	var id = req.param.id;
 
-// Create POST
+	Task.findByIdAndRemove(id, function(err){
+		if(err) console.log(err);
+		res.json(true);
+	});
+});
 
-// Done POST
-
-// Delete POST
-
-// Undo POST
 
 
 module.exports = router;
